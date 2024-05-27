18fe5974e276bbb42f43b4756e6e117958de983b
ThiagoAzeva <thiagoazeveddo@gmail.com>
2024-05-27 14:37:57 -0300

    primeiro git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
+    RodapeComponent,
 
   ],
   imports: [
